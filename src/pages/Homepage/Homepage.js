2ba3f03ffd600c5fdf59b9bfcd400c778054b965
Taylor Zanke <taylor.zanke@gmail.com>
2020-11-06 18:06:55 -0800

    remove

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -6,17 +6,7 @@ import PageContainer from '../../components/PageContainer/PageContainer'
 const Homepage = () => (
   <PageContainer className="homepage">
     <Grid container className="page-section">
-      <Grid item xs={6}>Taylor Zanke</Grid>
-      <Grid item xs={6}>Links</Grid>
-    </Grid>
-    <Grid container className="page-section">
-      <Grid item xs={12}>
-        <ul>
-          <li><a href="https://www.taylorzanke.com/">Artist Books</a></li>
-          <li><a href="https://taylorzanke.bandcamp.com/releases">Recordings</a></li>
-          <li><a href="https://www.littleuniverse.com/">Product Design</a></li>
-        </ul>
-      </Grid>
+      <Grid item xs={12}>taylor zanke . info</Grid>
     </Grid>
   </PageContainer>
 );
